a6ba0070e44bc6c663a92b2a04625adcb37f7f82
syed-burhan <syedhsjb@gmail.com>
2024-06-12 23:07:52 +0530

    Unit testcases for update Subscriptions

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -42,8 +42,9 @@ RSpec.describe SubscriptionsController, type: :controller do
                     expect(Subscription.count).to be(1)
                 end
 
-                it "returns Unprocessable entity" do
+                it "returns Unprocessable entity and error message" do
                     expect(response.code).to eq('422')
+                    expect(response.body).to eq("[\"Email has already been taken\"]")
                 end
             end
 
@@ -54,7 +55,8 @@ RSpec.describe SubscriptionsController, type: :controller do
                     post :create, params: { subscription: invalid_email_attributes }
                 end
 
-                it "returns Unprocessable entity" do
+                it "returns Unprocessable entity and error message" do
+                    expect(response.code).to eq('422')
                     expect(response.body).to eq("[\"Email is invalid\"]")
                 end
 
@@ -70,7 +72,8 @@ RSpec.describe SubscriptionsController, type: :controller do
                     post :create, params: { subscription: invalid_attributes }
                 end
 
-                it "returns Unprocessable entity" do
+                it "returns Unprocessable entity and error message" do
+                    expect(response.code).to eq('422')
                     expect(response.body).to eq("[\"Name can't be blank\",\"Email can't be blank\",\"Email is invalid\"]")
                 end
 
@@ -86,12 +89,100 @@ RSpec.describe SubscriptionsController, type: :controller do
                     post :create, params: { subscription: invalid_attributes }
                 end
 
-                it "returns Unprocessable entity" do
+                it "returns Unprocessable entity and error message" do
+                    expect(response.code).to eq('422')
                     expect(response.body).to eq("[\"Name can't be blank\",\"Email is invalid\"]")
                 end
 
                 it "does not create a new Subscription" do
                     expect(Subscription.count).to be(0)
+                end
+            end
+        end
+    end
+
+    
+    describe "PUT #update" do
+        context "with valid parameters" do
+            before :each do
+                Subscription.delete_all
+                @subscription = FactoryBot.create(:subscription)
+            end
+
+            let(:new_attributes) { { name: "John Wick II", email: "johnwick@example.com" } }
+
+            before :each do
+                put :update, params: { id: @subscription.id, subscription: new_attributes }
+            end
+
+            it "renders a JSON response with the Subscription" do
+                expect(response).to have_http_status(:ok)
+                expect(response.code).to eq('200')
+                expect(response.content_type).to include('application/json')
+            end
+
+            it "updates the same Subscription" do
+                expect(Subscription.count).to be(1)
+            end     
+        end
+
+        context "with invalid parameters" do
+            before do
+                Subscription.delete_all
+                @subscription = FactoryBot.create(:subscription)
+            end
+
+            context "with duplicate email" do
+                let(:valid_attributes) { { name: "John Wick II", email: "johnwick2@example.com" } }
+                let(:duplicate_email_attributes) { { name: "John Wick", email: "johnwick2@example.com" } }
+
+                before do
+                    post :create, params: { subscription: valid_attributes }
+                    put :update, params: { id: @subscription.id, subscription: duplicate_email_attributes }
+                end
+
+                it "returns Unprocessable entity and error message" do
+                    expect(response.code).to eq('422')
+                    expect(response.body).to eq("[\"Email has already been taken\"]")
+                end
+            end
+
+            context "with invalid email" do
+                let(:invalid_email_attributes) { { name: "John Wick", email: "johnwick@ex@mple.com" } }
+
+                before do
+                    put :update, params: { id: @subscription.id, subscription: invalid_email_attributes }
+                end
+
+                it "returns Unprocessable entity and error message" do
+                    expect(response.code).to eq('422')
+                    expect(response.body).to eq("[\"Email is invalid\"]")
+                end
+            end
+    
+            context "with empty name and email" do
+                let(:invalid_attributes) { { name: "", email: "" } }
+
+                before do
+                    put :update, params: { id: @subscription.id, subscription: invalid_attributes }
+                end
+
+                it "returns Unprocessable entity and error message" do
+                    expect(response.code).to eq('422')
+                    expect(response.body).to eq("[\"Name can't be blank\",\"Email can't be blank\",\"Email is invalid\"]")
+                end
+            end
+    
+            context "with empty name and invalid email" do
+                let(:invalid_attributes) { { name: "", email: "johnwick@ex@mple.com" } }
+
+                before do
+                    put :update, params: { id: @subscription.id, subscription: invalid_attributes }
+                end
+
+                it "returns Unprocessable entity and error message" do
+                    expect(response.code).to eq('422')
+                    expect(response.body).to eq("[\"Name can't be blank\",\"Email is invalid\"]")
                 end
             end
         end
